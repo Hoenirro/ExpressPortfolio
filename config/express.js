f5e269b34fe77d00ee6e6cf2f9e0e69a1f030521
Hoenirro <128835182+Hoenirro@users.noreply.github.com>
2023-09-25 22:42:36 -0400

    "another tiny mod"

--- config/express.js
+++ config/express.js
@@ -1,9 +1,24 @@
-let express = require('express');
+let express = require('express'),
+morgan = require('morgan'),
+compress = require('compression'),
+bodyParser = require('body-parser'),
+methodOverride = require('method-override');
 
 
 module.exports = function () {
     var app = express();
 
 
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
+     app.use('/', require('../app/routes/index.server.routes.js'));
+    //require('../app/routes/index.server.routes.js')(app);
+    app.use(express.static('./public'));
+    app.use(express.static("./node_modules"));
     return app;
 };
