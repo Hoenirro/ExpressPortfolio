ada9fe8b7ef9ff7e38256c8e64bab7a27bad87cf
Hoenirro <128835182+Hoenirro@users.noreply.github.com>
2023-09-26 15:13:40 -0400

    First Step Assignment

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-var express = require('./config/express');
+var express = require('express');
 var app = express();
-app.listen(5000);
-module.exports = app;
 
-console.log('Server running at http://localhost:5000/');
+app.use(express.static('./public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+// projects page
+app.get('/projects', function(req, res) {
+  res.render('pages/projects');
+});
+
+// Services page
+app.get('/services', function(req, res) {
+  res.render('pages/services'); // Renders the "services.ejs" template
+});
+
+// Contact page
+app.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
+app.listen(8080);
+console.log('Server is listening on http://localhost:8080/');
